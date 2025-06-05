f2b73045ac397ec2b8463ff524f593234d830b1a
leandro <lportocarrerocr@ucvvirtual.edu.pe>
2025-06-05 12:43:43 -0500

    modificacion rutas

--- sistemaedu/sistemaedu/urls.py
+++ sistemaedu/sistemaedu/urls.py
@@ -21,14 +21,17 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='public_home.html'), name='public_home'),  # Esta es la página principal
-    path('login/', include('home.urls')),
-    path('cargar/', include('cargarcontenido.urls')),
-    path('usuario/', include('usuario.urls')),
-    path('gestionarcontenido/', include('gestionarcontenido.urls')),
-    path('estadistica/', include('estadistica.urls')),
-    path('contenidoalumno/', include('contenidoalumno.urls')),
+    path('login/', include('home.urls')),  # Redirigir a /login
+    path('', include('home.urls')),
+    path('', include('cargarcontenido.urls')),
+    path('', include('usuario.urls')),
+    path('', include('gestionarcontenido.urls')),
+    path('', include('estadistica.urls')),
+    path('', include('contenidoalumno.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')), #new
+
+
 
    # Ruta para el login del estudiante 
     path('login_estudiante/', TemplateView.as_view(template_name='registration/login_estudiante.html'), name='login_estudiante'),
